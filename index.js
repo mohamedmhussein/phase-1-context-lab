e0b011f08acde0983e9515cc6942cf006cb39a3e
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-05-08 05:16:29 -0400

    context lab is done

--- index.js
+++ index.js
@@ -21,3 +21,85 @@ const allWagesFor = function () {
     return payable
 }
 
+
+function createEmployeeRecord(recordsArray){
+    return {
+        firstName: recordsArray[0],
+        familyName: recordsArray[1],
+        title: recordsArray[2],
+        payPerHour: recordsArray[3],
+        timeInEvents: [],
+        timeOutEvents:[]
+    }
+}
+function createEmployeeRecords(array){
+    let newArray = []
+    let i = 0
+    for (let element of array){
+        newArray[i] = createEmployeeRecord(element)
+        i++
+    }
+    return newArray
+
+}
+
+function createTimeInEvent(time){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time.substr(11, 4)),
+        date: time.substr(0, 10)
+    })
+
+    return this
+}
+
+function createTimeOutEvent(time){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time.substr(11, 4)),
+        date: time.substr(0, 10)
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(inputDate){
+    let hoursWorked = 0
+    for (let i = 0; i < this.timeInEvents.length; i++){
+        if (this.timeInEvents[i].date === inputDate){
+            hoursWorked = ((this.timeOutEvents[i].hour)-(this.timeInEvents[i].hour))/100
+            break
+        }
+    }
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(inputDate){
+    return hoursWorkedOnDate.call(this, inputDate)*this.payPerHour
+}
+
+// function allWagesFor(employeeRecord){
+//     let pay = 0
+//     for (let i = 0 ; i < employeeRecord.timeInEvents.length; i++){
+//         pay += wagesEarnedOnDate(employeeRecord, employeeRecord.timeInEvents[i].date)
+//     }
+//     return pay
+    
+// }
+
+function calculatePayroll(array){
+    let payroll = array.reduce( (acc, record) => {
+        return allWagesFor.call(record) + acc
+    }, 0)
+
+    // for (let employeeRecord of array){
+    //     pay += allWagesFor.call(employeeRecord)
+    // }
+     return payroll
+}
+
+function findEmployeeByFirstName(record, name){
+    const found = record.find(record => record.firstName === name)
+    return found
+
+}
